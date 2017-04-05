9c2c5f273886c08ce25e79740796fb590aa1bec4
intersignature <intersignature@hotmail.com>
2017-04-05 23:43:48 +0700

    signupServlet
    โค๊ดครบแล้วแต่รันไม่ล่ายยย

--- src/java/SignupServlet.java
+++ src/java/SignupServlet.java
@@ -6,11 +6,18 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 /**
  *
@@ -18,6 +25,10 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(urlPatterns = {"/SignupServlet"})
 public class SignupServlet extends HttpServlet {
+
+    @Resource(name = "dbesport")
+    private DataSource dbesport;
+    private Connection connection;
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -28,20 +39,46 @@ public class SignupServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    
+    public void init(){
+        try {
+            connection = dbesport.getConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger(SignupServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet SignupServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet SignupServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+            String fname = request.getParameter("fname");
+            String lname = request.getParameter("lname");
+            String email = request.getParameter("email");
+            String fb = request.getParameter("fb");
+            String university = request.getParameter("university");
+            String faculty = request.getParameter("faculty");
+            int phone = Integer.parseInt(request.getParameter("phone"));
+            String ign = request.getParameter("ign");
+            String sql = " insert into db_accessadmin.Player (P_Username, P_Password, P_FName, P_LName, P_Ign, P_Email, P_Facebook, P_Faculty, P_University, P_Phone)"+ 
+                    " values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, "aa");
+            stmt.setString(2, "aa");
+            stmt.setString(3, "aa");
+            stmt.setString(4, "aa");
+            stmt.setString(5, "aa");
+            stmt.setString(6, "aa");
+            stmt.setString(7, "aa");
+            stmt.setString(8, "aa");
+            stmt.setInt(9, 287413254);
+            stmt.setString(10, "aa");
+            stmt.execute();
+            out.println("zxdfsd");
+        } catch (SQLException ex) {
+            Logger.getLogger(SignupServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
