343634961b901ae106856f1672678d1dd8fb4758
intersignature <intersignature@hotmail.com>
2017-04-26 20:30:55 +0700

    ก่อนเทสระบบ

---
+++ build/generated/src/org/apache/jsp/CreateTeamSuccess_jsp.java
@@ -0,0 +1,226 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class CreateTeamSuccess_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\r\n");
+      out.write("<!DOCTYPE html>\r\n");
+      out.write("<html>\r\n");
+      out.write("\r\n");
+      out.write("    <head>\r\n");
+      out.write("        <meta charset=\"utf-8\">\r\n");
+      out.write("        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n");
+      out.write("        <title>lastest</title>\r\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets/bootstrap/css/bootstrap.min.css\">\r\n");
+      out.write("        <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Cookie\">\r\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets/fonts/font-awesome.min.css\">\r\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets/css/styles.css\">\r\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets/css/Pretty-Footer.css\">\r\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets/css/Google-Style-Login.css\">\r\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets/css/Pretty-Registration-Form.css\">\r\n");
+      out.write("        <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/bootstrap-treeview/1.2.0/bootstrap-treeview.min.css\">\r\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets/css/tree.css\">\r\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets/css/nav-1.css\">\r\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets/css/tab1.css\">\r\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets/css/tour.css\">\r\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets/css/tournament.css\">\r\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets/css/info-tab_player001.css\">\r\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets/css/player_001.css\">\r\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets/css/player_tab.css\">\r\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets/css/search.css\">\r\n");
+      out.write("    </head>\r\n");
+      out.write("\r\n");
+      out.write("    <body>\r\n");
+      out.write("       ");
+ if (session.getAttribute("suc") == null || (int) session.getAttribute("suc") == 0) { 
+      out.write("\r\n");
+      out.write("        ");
+
+            int suc = 0;
+            session.setAttribute("suc", suc);
+            response.sendRedirect("indexJSP.jsp");
+        
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("        ");
+ } else if ((int) session.getAttribute("suc") == 1) {
+      out.write("\r\n");
+      out.write("        <header>\r\n");
+      out.write("            <!-- Second navbar for sign in -->\r\n");
+      out.write("            <nav class=\"navbar navbar-default\" id=\"headnav\">\r\n");
+      out.write("                <div class=\"container\">\r\n");
+      out.write("                    <!-- Brand and toggle get grouped for better mobile display -->\r\n");
+      out.write("                    <div class=\"navbar-header\" >\r\n");
+      out.write("                        <button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#navbar-collapse-2\">\r\n");
+      out.write("                            <span class=\"sr-only\">Toggle navigation</span>\r\n");
+      out.write("                            <span class=\"icon-bar\"></span>\r\n");
+      out.write("                            <span class=\"icon-bar\"></span>\r\n");
+      out.write("                            <span class=\"icon-bar\"></span>\r\n");
+      out.write("                        </button>\r\n");
+      out.write("                        <a href=\"indexJSP.jsp\">\r\n");
+      out.write("                            <img src=\"assets/img/logo.png\"  id = \"logo\">\r\n");
+      out.write("                            <a class=\"navbar-brand\" href=\"indexJSP.jsp\">U-LEAGUE</a>\r\n");
+      out.write("                    </div>\r\n");
+      out.write("                    <ul class=\"nav navbar-nav navbar-right\" id=\"userbar\">\r\n");
+      out.write("                        ");
+ session = request.getSession();
+                            String username = (String) session.getAttribute("username");
+                            String imgdir = (String) session.getAttribute("useimage"); ;
+                            int id = (int) session.getAttribute("P_ID");
+                        
+      out.write("\r\n");
+      out.write("                         <li id = \"searchnav2\"> \r\n");
+      out.write("                  <a>\r\n");
+      out.write("                      <form action=\"searchServlet\" method=\"POST\" >\r\n");
+      out.write("                      <input id = \"searchbox\" type=\"text\" name=\"searchuser\" placeholder=\"Username..\">\r\n");
+      out.write("                    \r\n");
+      out.write("                      <input id = \"searchbtn\" type=\"submit\" value=\"Search\" />\r\n");
+      out.write("                       </form>\r\n");
+      out.write("                  </a>\r\n");
+      out.write("              </li>\r\n");
+      out.write("                        <a href=\"Player_001Servlet?player=");
+out.println(id);
+      out.write("\"> \r\n");
+      out.write("                            <li id=\"namepro\">\r\n");
+      out.write("\r\n");
+      out.write("                                <img src= \"");
+ out.println(imgdir); 
+      out.write("\" id=\"imgpro\">\r\n");
+      out.write("                                <div id = \"namepro2\">\r\n");
+      out.write("                                    <h4><b>");
+ out.println(username); 
+      out.write("</b></h4>\r\n");
+      out.write("                                </div>\r\n");
+      out.write("\r\n");
+      out.write("                            </li>\r\n");
+      out.write("                        </a>\r\n");
+      out.write("\r\n");
+      out.write("                        <form action=\"SignoutServlet\" method=\"POST\" class=\"navbar-form navbar-right form-inline\" role=\"form\">\r\n");
+      out.write("                            <input type=\"hidden\" name=\"from\" value=\"indexJSP.jsp\" />\r\n");
+      out.write("                            <li >\r\n");
+      out.write("                                <button type=\"submit\" class=\"btn btn-default btn-outline btn-circle collapsed\"  id=\"signinbtn\" >Sign Out</button>\r\n");
+      out.write("\r\n");
+      out.write("                            </li>\r\n");
+      out.write("                        </form>\r\n");
+      out.write("                    </ul>\r\n");
+      out.write("\r\n");
+      out.write("                </div><!-- /.container -->\r\n");
+      out.write("            </nav><!-- /.navbar -->\r\n");
+      out.write("        </header>\r\n");
+      out.write("        ");
+ }
+      out.write("\r\n");
+      out.write("        <ul class=\"nav nav-pills categories\">\r\n");
+      out.write("            <li id=\"menu\"><a href=\"newsJSP.jsp\" id=\"fontmenu\">NEWS </a></li>\r\n");
+      out.write("            <li id=\"menu\"><a href=\"TourJSP.jsp\" id=\"fontmenu\">TOURNAMENT </a></li>\r\n");
+      out.write("            <li id=\"menu\"><a href=\"TeamServlet\" id=\"fontmenu\">TEAM </a></li>\r\n");
+      out.write("            <li id=\"menu\"><a href=\"PlayerServlet\" id=\"fontmenu\">PLAYERS </a></li>\r\n");
+      out.write("            <li id=\"menu\"><a href=\"rulesJSP.jsp\" id=\"fontmenu\">RULES </a></li>\r\n");
+      out.write("            <li id=\"menu\"><a href=\"faqJSP.jsp\" id=\"fontmenu\">FAQ </a></li>\r\n");
+      out.write("            <li id=\"menu\"><a href=\"tourRequestJSP.jsp\" id=\"fontmenu\">TOURNAMENT REQUEST </a></li>\r\n");
+      out.write("        </ul>\r\n");
+      out.write("        <div class=\"container\">\r\n");
+      out.write("            <div class=\"row register-form\">\r\n");
+      out.write("                <div class=\"col-md-8 col-md-offset-2\">\r\n");
+      out.write("                    <form action=\"searchServlet\" method=\"POST\">\r\n");
+      out.write("                   \r\n");
+      out.write("                    <h1>Your Team Created</h1>\r\n");
+      out.write("                    <div id = \"searchpos\">\r\n");
+      out.write("                        <div class=\"row\" >\r\n");
+      out.write("\r\n");
+      out.write("                            <div class=\"search\">\r\n");
+      out.write("                                <input type=\"text\" class=\"form-control input-sm\" maxlength=\"64\" placeholder=\"Username\" name=\"searchuser\"/>\r\n");
+      out.write("                                <button type=\"submit\" class=\"btn btn-primary btn-sm\">Search</button>\r\n");
+      out.write("                            </div>\r\n");
+      out.write("                        </div>\r\n");
+      out.write("                    </div>\r\n");
+      out.write(" </form>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("                </div>  \r\n");
+      out.write("            </div>\r\n");
+      out.write("        </div>\r\n");
+      out.write("        \r\n");
+      out.write("<footer id=\"footer001\">\r\n");
+      out.write("        <div class=\"row\">\r\n");
+      out.write("            <div class=\"col-md-4 col-sm-6 footer-navigation\">\r\n");
+      out.write("                <h3><a href=\"#\">U-LEAGUE<span><img src=\"assets/img/logo.png\" id=\"footlogo\"> </span></a></h3>\r\n");
+      out.write("            </div>\r\n");
+      out.write("            <div class=\"col-md-4 col-sm-6 footer-contacts\">\r\n");
+      out.write("                <div><i class=\"fa fa-facebook footer-contacts-icon\" ></i>\r\n");
+      out.write("                    <p class=\"footer-center-info email text-left\"> <a href=\"https://www.facebook.com/ULeagueTH/\">U LEAUGE</a></p>\r\n");
+      out.write("                </div>\r\n");
+      out.write("\r\n");
+      out.write("                <div><i class=\"fa fa-envelope footer-contacts-icon\"></i>\r\n");
+      out.write("                    <p> <a>lawslifeaways@gmail.com</a></p>\r\n");
+      out.write("                </div>\r\n");
+      out.write("            </div>\r\n");
+      out.write("            <div class=\"clearfix visible-sm-block\"></div>\r\n");
+      out.write("            <div class=\"col-md-4 footer-about\">\r\n");
+      out.write("                <h4>U LEAUGE</h4>\r\n");
+      out.write("                <p> A e-sport tournament of university.For relationship, good ethics, unity and increase performance of e-sport tournament.\r\n");
+      out.write("                </p>\r\n");
+      out.write("            </div>\r\n");
+      out.write("        </div>\r\n");
+      out.write("    </footer>\r\n");
+      out.write("        <script src=\"assets/js/jquery.min.js\"></script>\r\n");
+      out.write("        <script src=\"assets/bootstrap/js/bootstrap.min.js\"></script>\r\n");
+      out.write("        <script src=\"https://cdnjs.cloudflare.com/ajax/libs/bootstrap-treeview/1.2.0/bootstrap-treeview.min.js\"></script>\r\n");
+      out.write("        <script src=\"assets/js/treejs.js\"></script>\r\n");
+      out.write("        <script src=\"assets/js/popupjs.js\"></script>\r\n");
+      out.write("    </body>\r\n");
+      out.write("\r\n");
+      out.write("</html>");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
